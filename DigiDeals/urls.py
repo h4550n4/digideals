e3710b1d3348a0f89206dd0f53f843db2fb0ada3
h4550n4 <ahmedhassanein723@gmail.com>
2024-09-05 05:56:35 +0300

    update

--- DigiDeals/urls.py
+++ DigiDeals/urls.py
@@ -22,7 +22,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('products.urls')),
-    path('', include('categories.urls'))
+    path('categories/', include('categories.urls'))
 
 ]
 
--- categories/urls.py
+++ categories/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns= [
-    path('categories', views.category_list, name='category_list'),
-    path('categories/<int:category_id>', views.category_products, name='category_products'),
+    path('', views.category_list, name='category_list'),
+    path('<int:category_id>', views.category_products, name='category_products'),
 ]
